443813488d29a00dd0f6e90be7df0abc697420c1
Chetan S <chetan.subramanya@wisseninfotech.com>
2022-01-13 22:18:19 +0530

    added git commands in Test java file

--- src/com/codes/Test.java
+++ src/com/codes/Test.java
@@ -4,7 +4,14 @@ public class Test {
 
 	public static void main(String[] args) {
 	System.out.println("modified this");
-
+/*  
+    git init
+	git add README.md
+	git commit -m "first commit"
+	git branch -M main
+	git remote add origin https://github.com/chetanchethu/r.git
+	git push -u origin main
+	*/
 	}
 
 }
