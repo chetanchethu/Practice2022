bbc44e22aab54964bae11b744f2e1c006afccad0
Chetan S <chetan.subramanya@wisseninfotech.com>
2022-02-03 12:27:49 +0530

    modified

--- src/com/codes/PrintLostWord.java
+++ src/com/codes/PrintLostWord.java
@@ -9,7 +9,8 @@ public class PrintLostWord {
 	{
 	String string = "Hello,This,is,the,lost,word";//word
 	System.out.println(string);
-    System.out.println("Last word is :"+string.substring (string.lastIndexOf (','),string.length())
+    System.out.println("Last word is :"+string.substring 
+    		(string.lastIndexOf (','),string.length())
     .replace(',', '\s').trim());
 	
     System.out.println(string.split(",").length);//No of words
